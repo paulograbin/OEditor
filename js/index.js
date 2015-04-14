cc4971c8abd910ddf4831a4c43f21b33bf33d054
Paulo Gräbin <plgrabin@gmail.com>
2015-04-14 16:55:34 -0300

    Corrigindo alguns problemas, agora já é salvar e abrir algum texto.

--- js/index.js
+++ js/index.js
@@ -5,63 +5,50 @@ app.controller('EditorTextoCtrl', function($scope) {
 	$scope.botaoSalvarHabilitado = false;
 	
 	var getConteudo = function () {
-		if(!editor) return;
 		// Get editor contents
+        var editor = CKEDITOR.instances.editor1;
+        console.log("Texto retornado: " + editor.getData());
+        
 		return editor.getData();
 	};
 	
 	$scope.listaTextos = [];
 	$scope.adicionarTextoSalvo = function () {
 		count = count + 1;
+        console.log("Função: adicionarTextoSalvo no index " + count);
 		
 		var texto = {};
 		texto.conteudo = getConteudo();
 		texto.nome = "teste" + count;
-		$scope.listaTextos.push(texto);
-//		$scope.removerEditor();
-	};
-	
-	// Exibe o editor
-	$scope.criarEditor = function () {
-		if (editor)	return;
+        
+        
 
-		// Create a new editor inside the <div id="editor">, setting its value to html
-		var config = {};
-		editor = CKEDITOR.appendTo('editor', config, html);
-		$scope.botaoSalvarHabilitado = true;
+		$scope.listaTextos.push(texto);
 	};
-	
-//	$scope.removerEditor = function () {
-//		if (!editor)
-//			return;
-//		
-//		document.getElementById('editorcontents').innerHTML = html = editor.getData();
-//		document.getElementById('contents').style.display = '';
-//
-//		// Destroy the editor.
-//		editor.destroy(true);
-//		editor = null;
-//		$scope.botaoSalvarHabilitado = false;
-//	};
 	
 	$scope.abrirTexto = function (index) {
+        console.log("Função: abrirTexto, index: " + index+1);
 		var textValue = $scope.listaTextos[index];
-		SetContents(textValue);
+		setContents(textValue);
 	};
 	
 	$scope.apagarTexto = function (index) {
 		$scope.listaTextos.splice(index, 1);
 	};
+    
+    $scope.clearContents = function () {
+        setContents("");
+    }
 	
 	// Seta conteúdo no editor
-	function SetContents(text) {
+	function setContents(text) {
+        console.log("Função: setContents");
+        console.log("Texto settado: " + text);
 		// Get the editor instance that we want to interact with.
 		var editor = CKEDITOR.instances.editor1;
-//		var value = document.getElementById('htmlArea').value;
 
 		// Set editor contents (replace current contents).
 		// http://docs.ckeditor.com/#!/api/CKEDITOR.editor-method-setData
 		editor.setData(text);
 	}
-
 });
