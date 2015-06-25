e4eec3d4ead6bae457dc2cdf8f9f98d562508058
matheusbloebaum <matheus.bloebaum@gmail.com>
2015-06-25 20:11:16 -0300

    Ajuste de layout e criação de login

---
+++ public/javascripts/global.js
@@ -0,0 +1,15 @@
+function global.login(){
+	alert('d');
+}
+
+module.exports = {
+  foo: function () {
+    alert('d');
+  },
+  bar: function () {
+    alert('da');
+  }
+};
+
+var zemba = function () {
+}
