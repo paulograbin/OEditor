b039725000b9fff3092db3b866a278be1f3de2f8
Alexandre Dal Zotto <alexandre.dzh@gmail.com>
2015-06-26 00:00:54 -0300

    Primeira versão da operação de salvar

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,17 +1,28 @@
 console.log("Inicializou o global.js");
+$(document ).ready(function() {
 
-var login = function (){
-	window.location='http://www.google.com';
-}
+	$('#btnSave').on('click', function(){
+ 
+        // If it is, compile all user info into one object
+        var note = {
+            'text': $("#editor1").val()
+        }
 
-var exports = {
-  foo: function () {
-    alert('d');
-  },
-  bar: function () {
-    alert('da');
-  }
-};
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: note,
+            url: '/addnote',
+            dataType: 'JSON'
+        }).done(function( response ) {
+            if (response.msg === '') {
+            	alert("Registro inserido com sucesso!");	
+            }
+        	
+        });
 
-var zemba = function () {
-}
+	});
+		
+});
+
+
