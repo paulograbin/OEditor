6f2631f0298bbea2998f8ebb1d3ee8114ce641ab
Paulo Grabin <plgrabin@gmail.com>
2016-02-04 00:19:35 -0200

    Teste com nova porta.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000);
-console.log("OEditor está rodando na porta 3000");
+app.listen(4300);
+console.log("OEditor está rodando na porta 4300");
 
 module.exports = app;
